df24082c07956afa25a323ccba51eed7b3fb5ffc
Ankit9835 <ankydchrismatic@gmail.com>
2023-09-26 22:47:33 +0530

    blog like and unlike completed

--- app/dashboard/user/page.js
+++ app/dashboard/user/page.js
@@ -1,40 +1,41 @@
-"use-client"
-import { useState } from "react"
-import { useEffect } from "react"
-import ListBlog from "@/components/blog/ListBlog"
-import { NextResponse } from "next/server"
-import toast from "react-hot-toast"
-
-const [likedBlog, setLikedBlog] = useState([])
-
-const fetchLikedBlog = async () => {
-    try {
-        const response = await fetch(`${process.env.API}/user/liked-blogs`)
-        console.log('response',response)
-        if(!response.ok){
-           throw new Error('Failed to fetch blogs')
-        }
-        const data = await response.json()
-        setLikedBlog(response.data)        
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-useEffect(() => {
-    fetchLikedBlog()
-},[])
-
-
+"use client";
+import { useState, useEffect } from "react";
+import ListBlog from "@/components/blog/ListBlog";
+import { NextResponse } from "next/server";
+import toast from "react-hot-toast";
 
 export default function UserDashboard() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <p className="lead"> Dashboard </p>
-                </div>
-            </div>
+  const [likedBlog, setLikedBlog] = useState([]);
+  const fetchLikedBlog = async () => {
+    try {
+      const response = await fetch(`${process.env.API}/user/liked-blogs`);
+      console.log("response", response);
+      if (!response.ok) {
+        throw new Error("Failed to fetch blogs");
+      }
+      const data = await response.json();
+      console.log("data", data);
+      setLikedBlog(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLikedBlog();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <p className="lead">
+            Liked Blogs
+          </p>
+          <br />
+          <ListBlog blogs={likedBlog}/>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
